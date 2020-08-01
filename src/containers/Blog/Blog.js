57a69903b81fb304b20ce838e3812206fe24d575
Huguez <carlos.huguez@gmail.com>
2020-07-31 21:31:27 -0700

    se logro hacer la peticion get con axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import classes from './Blog.css';
 
 class Blog extends Component {
+    
+    state={
+        posts:[],
+        selectedId: null,
+    }
+    
+    componentDidMount(){
+        axios.get( 'https://jsonplaceholder.typicode.com/posts' ).then(
+            ( resp ) => {
+                const posts = resp.data.slice( 0, 4 );
+                const updatePosts = posts.map( ( post ) => { 
+                    return { ...post, author:'Huguez' } 
+                 } );
+                
+                this.setState( { posts: updatePosts } );
+            }
+        );
+    }
+    
+    postSelector = ( id ) => {
+        this.setState( { selectedId: id } )
+    }
+
+
     render () {
+        const posts = this.state.posts.map( ( p ) => { 
+            return < Post 
+                clicked={ () => this.postSelector( p.id )  }
+                key={ p.id }
+                title={ p.title } 
+                author={ p.author } />
+         });
         return (
             <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                <section className={classes.Posts}>
+                    { posts }
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={ this.state.selectedId }  />
                 </section>
                 <section>
                     <NewPost />
