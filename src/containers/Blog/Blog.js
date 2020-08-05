8b5d98d2dc01995e49da7ddf33ae56e0f001d57b
Huguez <carlos.huguez@gmail.com>
2020-08-04 21:48:39 -0700

    se introdujo el switch para arreglar el problema de rutas

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,14 +7,14 @@ import FullPost from './FullPost/FullPost';
 import './Blog.css';
 
 import classes from './Blog.css';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 class Blog extends Component {
 
 
 
     render () {        
-        // console.log(this.props);
+        
         return (
             <div className={ classes.Blog }>
                 <header>
@@ -35,9 +35,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={ Posts }/>
-                <Route path="/:id" exact component={ FullPost }/>
-                <Route path="/newPost" component={ NewPost }/>
+                <Switch>
+                    <Route path="/" exact component={ Posts }/>
+                    <Route path="/newPost" component={ NewPost }/>
+                    <Route path="/:id" exact component={ FullPost }/>
+                </Switch>
             </div>
         );
     }
