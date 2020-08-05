bd2f2d5657e251f5cbbc68b66a9b9a04eaf4ed7e
Huguez <carlos.huguez@gmail.com>
2020-08-04 17:31:43 -0700

    se cambiaron las rutas de componentes para aplicar el react-router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,63 +1,24 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import instance from '../../axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
+
 import classes from './Blog.css';
 
 class Blog extends Component {
-    
-    state={
-        posts:[],
-        selectedId: null,
-        error: false
-    }
-    
-    componentDidMount(){
-        instance.get( '/posts' ).then(
-            ( resp ) => {
-                const posts = resp.data.slice( 0, 4 );
-                const updatePosts = posts.map( ( post ) => { 
-                    return { ...post, author:'Huguez' } 
-                 } );
-                
-                this.setState( { posts: updatePosts } );
-            }
-        ).catch( err =>{
-            this.setState( { error: true } );
-        });
-    }
-    
-    postSelector = ( id ) => {
-        this.setState( { selectedId: id } )
-    }
-
 
     render () {
-        let posts = <p style={ { textAlign: 'center' } }> Algo Salio Mal!!!!! </p>;
         
-        if( !this.state.error ){
-            posts = this.state.posts.map( ( p ) => { 
-                return < Post 
-                    clicked={ () => this.postSelector( p.id )  }
-                    key={ p.id }
-                    title={ p.title } 
-                    author={ p.author } />
-            });
-        }
-
         return (
-            <div>
-                <section className={classes.Posts}>
-                    { posts }
-                </section>
-                <section>
-                    <FullPost id={ this.state.selectedId }  />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className={ classes.Blog }>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">New Post</a></li>
+                            <li><a href="/">link</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Posts />
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import instance from '../../../axios';
+
+import Post from '../../../components/Post/Post'
+import classes from "./Posts.css";
+
+class Posts extends Component{
+    
+    state={
+        posts:[],
+    }
+
+    componentDidMount(){
+        instance.get( '/posts' ).then(
+            ( resp ) => {
+                const posts = resp.data.slice( 0, 4 );
+                const updatePosts = posts.map( ( post ) => { 
+                    return { ...post, author:'Huguez' } 
+                 } );
+                
+                this.setState( { posts: updatePosts } );
+            }
+        ).catch( err =>{
+            console.log(err);
+            // this.setState( { error: true } );
+        });
+    }
+
+    postSelector = ( id ) => {
+        this.setState( { selectedId: id } )
+    }
+
+    render(){
+        
+        let posts = <p style={ { textAlign: 'center' } }> Algo Salio Mal!!!!! </p>;
+        
+        if( !this.state.error ){
+            posts = this.state.posts.map( ( p ) => { 
+                return < Post 
+                    clicked={ () => this.postSelector( p.id )  }
+                    key={ p.id }
+                    title={ p.title } 
+                    author={ p.author } />
+            });
+        }
+
+        return (
+            <section className={classes.Posts}>
+                { posts }
+            </section>
+        );
+    }
+}
+
+export default Posts;
